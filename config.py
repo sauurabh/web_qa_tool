ca4710b55015f68b140fad8249ea0d4d97b70e2d
Saurabh Kumar <77803374+sauurabh@users.noreply.github.com>
2025-02-25 13:01:33 +0530

    Update config.py
    updated config.py to get api key from streamlit secrets.toml

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
-API_KEY = os.getenv("OPENAI_API_KEY")
+# load_dotenv()
+# API_KEY = os.getenv("OPENAI_API_KEY")
+API_KEY = st.secrets["OPENAI_API_KEY"]
